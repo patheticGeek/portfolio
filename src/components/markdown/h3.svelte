eecdbd3c5aa8a5250e0bbdc30003161ec31850dd
Pathetic Geek <geekpathetic@gmail.com>
2022-01-03 08:17:27 +0530

    🐛 Put right h tag

--- src/components/markdown/h3.svelte
+++ src/components/markdown/h3.svelte
@@ -1,7 +1,7 @@
 <script>
-  import Wrapper from "./wrapper.svelte"
+	import Wrapper from './wrapper.svelte'
 </script>
 
 <Wrapper let:class={className} class="font-medium mb-3" symbol="###">
-  <h1 class={className}><slot /></h1>
+	<h3 class={className}><slot /></h3>
 </Wrapper>
